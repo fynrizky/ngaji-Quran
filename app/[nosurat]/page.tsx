98203dfdb4ec833f497c9e695cab346e5c7c1380
fynrizky <fiyaniky25@gmail.com>
2023-07-05 11:04:58 +0700

    update page modal tafsir

--- app/[nosurat]/page.tsx
+++ app/[nosurat]/page.tsx
@@ -89,7 +89,7 @@ export default function Page({ params }: { params: { nosurat: string } }) {
   }, [bukaAyat])
 
   React.useEffect(() => {
-    if(book?.url && book?.nomorAyat){
+    if(book?.url){
       dispatch(modalTafsir(`${book?.nomorAyat}`))
     }
   },[dispatch])

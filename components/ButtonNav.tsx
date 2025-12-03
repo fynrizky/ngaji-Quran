838295435b812b8ad90fe5a9fa6d1c41e82b347b
fynrizky <fiyaniky25@gmail.com>
2025-12-03 12:21:27 +0700

    tafsir updt

--- components/ButtonNav.tsx
+++ components/ButtonNav.tsx
@@ -106,7 +106,7 @@ export default function ButtonNav() {
                     book &&
                       dispatch(
                         modalLoading(
-                          `Menelusuri Tafsir Terakhir Surah ${book.namaSurat} ayat ${book.nomorAyat}`
+                          `Membuka Tafsir ${book.namaSurat} ayat ${book.nomorAyat}`
                         )
                       )
                     window.location.href = `${window.location.origin}${book.url}`
@@ -125,7 +125,7 @@ export default function ButtonNav() {
                     book &&
                     dispatch(
                       modalLoading(
-                        `Menelusuri Tafsir Tarakhir Surah ${book?.namaSurat} ayat ${book?.nomorAyat}`
+                        `Membuka Tafsir ${book?.namaSurat} ayat ${book?.nomorAyat}`
                       )
                     )
                   }}>
